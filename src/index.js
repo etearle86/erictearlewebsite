9232b92c51c96ae64e16afef1614788ef81683eb
etearle86 <e.tearle@gmail.com>
2026-06-18 21:25:33 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AppContextProvider } from "./context/AppContext";
-import BioStorybookWidget from "bio-storybook-widget";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppContextProvider>
-      <BioStorybookWidget projectSource={"bridget-baker"} />
       <App />
     </AppContextProvider>
   </React.StrictMode>
