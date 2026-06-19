ad1707bf215b8f0732ea920d3655f2824ccbc574
etearle86 <e.tearle@gmail.com>
2026-06-20 00:17:21 +0100

    Update Overlay.jsx

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Overlay.scss';
 
 const Overlay = ({ handlePrevClick, handleCloseClick, handleNextClick }) => {
+    const [currCursorPos, setCurrCursorPos] = useState({ x: 0, y: 0 });
+
+    const handleMouseMove = (ev) => {
+        let posX = ev.clientX;
+        let posY = ev.clientY;
+        setCurrCursorPos({
+            x: posX,
+            y: posY,
+        });
+    };
+
     return (
-        <div className='Overlay_wrapper'>
-            <button className='Overlay_prev' onClick={handlePrevClick}>←</button>
-            <button className='Overlay_close' onClick={handleCloseClick}>✕</button>
-            <button className='Overlay_next' onClick={handleNextClick}>→</button>
+        <div className='Overlay_wrapper' onMouseMove={handleMouseMove}>
+            <section className='prev_section' onClick={handlePrevClick}>
+                <div
+                    className='Overlay_cursor'
+                    style={{
+                        '--left': `${currCursorPos.x}px`,
+                        '--top': `${currCursorPos.y}px`,
+                    }}
+                />
+            </section>
+            <section className='close_section' onClick={handleCloseClick}>
+                <div
+                    className='Overlay_cursor'
+                    style={{
+                        '--left': `${currCursorPos.x}px`,
+                        '--top': `${currCursorPos.y}px`,
+                    }}
+                />
+            </section>
+            <section className='next_section' onClick={handleNextClick}>
+                <div
+                    className='Overlay_cursor'
+                    style={{
+                        '--left': `${currCursorPos.x}px`,
+                        '--top': `${currCursorPos.y}px`,
+                    }}
+                />
+            </section>
         </div>
     );
 };
